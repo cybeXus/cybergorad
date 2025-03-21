cc6cd6b0c69dbda36a13db81233b5dffa8f9fc39
cyberGorad <134638741+cyberGorad@users.noreply.github.com>
2025-03-21 05:59:18 +0000

    Update admin.py avec toutes formation chaques parties

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
-from .models import Formation
-from .models import Blog
+from .models import (
+    Formation_dev_web, Formation_cybersecurite,
+    Formation_reseau, Formation_dev_mobile, Formation_design, Blog
+)
 
 
-class FormationAdmin(admin.ModelAdmin):
+class FormationDevWebAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'date_creation', 'video')
+    search_fields = ('titre',)
+
+
+class FormationCybersecuriteAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'date_creation', 'video')
+    search_fields = ('titre',)
+
+
+class FormationReseauAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'date_creation', 'video')
+    search_fields = ('titre',)
+
+
+class FormationDevMobileAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'date_creation', 'video')
+    search_fields = ('titre',)
+
+
+class FormationDesignAdmin(admin.ModelAdmin):
     list_display = ('titre', 'date_creation', 'video')
     search_fields = ('titre',)
 
@@ -13,6 +35,10 @@ class BlogAdmin(admin.ModelAdmin):
     search_fields = ('titre',)
 
 
-# Enregistrement des modèles avec leurs administrateurs
-admin.site.register(Formation, FormationAdmin)
+# Enregistrement des modèles dans l'admin Django
+admin.site.register(Formation_dev_web, FormationDevWebAdmin)
+admin.site.register(Formation_cybersecurite, FormationCybersecuriteAdmin)
+admin.site.register(Formation_reseau, FormationReseauAdmin)
+admin.site.register(Formation_dev_mobile, FormationDevMobileAdmin)
+admin.site.register(Formation_design, FormationDesignAdmin)
 admin.site.register(Blog, BlogAdmin)
